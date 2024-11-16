b24a298cedcc95520ebe0ff11e349286e569569e
MariEche <mariaecheverris@oakland.edu>
2024-11-16 13:51:05 -0500

    added more of server code

--- ServerBackend.py
+++ ServerBackend.py
@@ -1,5 +1,5 @@
 #backend to handle the server side functionality
-
+# note from Maria: wrote more of the server code not sure if it works yet havent tested
 import socket
 from threading import Thread
 import os
@@ -10,21 +10,55 @@ class ServerBackend: # constructor
     Clients = []
 
     def __init__(self, HOST, PORT):
-        return # placeholder to avoid errors
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.bind((HOST, PORT))
+        self.socket.listen(5)
+        print('Initiating Chat...')
+        #return # placeholder to avoid errors
     
 def listenForConnections(self):
+    while True:
+        client_socket, adress = self.socket.accept()
+        print("Recieved connection with:" + str(adress))
+
+        ClientName = client_socket.recv(1024).decode()
+        client = {'ClientName': ClientName, 'client_socket': client_socket}
+
+        self.broadcast_message(ClientName, "Say hi to" + ClientName + "!")
+
+        Server.Clients.append(client)
+        Thread(target = configureNewClient, args = (client,)).start()
 # listen for requests
-    return #another placeholder
+    #return #another placeholder
 
 def configureNewClient(self, client):
+    ClientName = client['client_name']
+    client_socket = client['client_socket']
+    while True:
+        clientMessage = client_socket.recv(1024).decode()
+
+        if client_message.strip() == ClientName + ": bye" or not clientMessage.strip():
+            self.sendMessage(ClientName, ClientName + "disconnected from chat")
+            Server.clients.remove(client)
+            client_socket.close()
+            break
+        else:
+            self.sendMessage(ClientName, clientMessage)
 #establish connection with a new client
     return
 
-def sendMessage(self, clientName, message):
+def sendMessage(self, clientSending, message):
+    for client in self.Clients:
+        client_socket = client['client_socket']
+        ClientName = client['ClientName']
+        if ClientName != clientSending:
+            client_socket.send(message.encode())
 # send message prompted by client
     return
 
 if __name__ == 'main':
+    #server = Server('127.0.0.1' 7632)
+    #server.listen()
     #server stuff here
     # may need client IPs here
     ServerBackend(IP, 3232) # any socket number 1025 - 65536
