d645f349d70aacccedd94d34e09f05edfe188e16
HaadiaSiddiqui <haadiajaved@gmail.com>
2024-11-23 22:45:08 -0500

    Update ClientGUI.py
    connects to ClientBackend to send and receive messages from the server

--- ClientGUI.py
+++ ClientGUI.py
@@ -1,8 +1,25 @@
 import kivy
 
 class ClientGUI(ServerClientGUI):
-    def __init__():
-        # add elements specific to Client GUI
+        def __init__(self, client_backend, **kwargs):
+        super().__init__(**kwargs)
+        self.orientation = "vertical"
+        self.client_backend = client_backend
+    
+#to display chat
+    self.chat_display = Label(size_hint_y=None, height=500, text="", valign="top", halign="left")
+        self.chat_display.bind(size=self.update_chat_size)
+        scroll_view = ScrollView(size_hint=(1, None), size=(self.width, 500))
+        scroll_view.add_widget(self.chat_display)
+        self.add_widget(scroll_view)
+
+    self.input_field = TextInput(size_hint_y=None, height=40, multiline=False, hint_text="Type a message")
+        self.add_widget(self.input_field)
+
+    self.send_button = Button(text="Send", size_hint_y=None, height=40)
+        self.send_button.bind(on_press=self.send_message)
+        self.add_widget(self.send_button)
+
         return
     
 def loadClientElements():
