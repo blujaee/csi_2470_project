dbe55c343ee22c64273b87e1438758d7ad95cfe9
Emily Lasecki <emilylasecki@oakland.edu>
2024-12-02 16:53:16 -0500

    added more comments to clientBackend

--- ClientBackend.py
+++ ClientBackend.py
@@ -3,23 +3,24 @@
 import socket
 from threading import Thread
 import os
+from kivy.clock import Clock
 
 from ClientsIP import IP
 
 
 class ClientBackend:
 
-    def __init__(self, HOST, PORT, gui_mode=False, gui_app=None, username=None):
-        self.socket = socket.socket()
-        self.gui_mode = gui_mode
+    def __init__(self, HOST, PORT, gui_mode=False, gui_app=None, username=None): # constructor
+        self.socket = socket.socket() # create instance of socket class
+        self.gui_mode = gui_mode # control whether gui or terminal mode
         self.gui_app = gui_app
         try:
-            self.socket.connect((HOST, PORT))
+            self.socket.connect((HOST, PORT)) # try to connect to socket, else warn user server status invalid
             if self.gui_mode:
                 self.name = username  # Use the username provided by the GUI
             else:
-                self.name = input("Enter your name: ")
-            self.connectToServer()
+                self.name = input("Enter your name: ") # use username typed in terminal
+            self.connectToServer() # connect to server
         except Exception as e:
             print(f"error: server not running ({e})")
             os._exit(0)
@@ -27,18 +28,18 @@ class ClientBackend:
     def connectToServer(self):
         self.socket.send(self.name.encode()) # send name down the socket once the user has input it
         Thread(target=self.recieveMessage, daemon=True).start() # start listening for messages
-        if not self.gui_mode: # if the GUI isn't active, can still run in terminal via a different method
-            self.sendMessageLoop()
+        if not self.gui_mode: 
+            self.sendMessageLoop() # terminal mode send message
 
     def recieveMessage(self):
         while True: #always listen for messages
             try:
                 serverMessage = self.socket.recv(1024).decode() #recieve message on the socket
-                if not serverMessage.strip(): # if server couldn't be connected to
+                if not serverMessage.strip(): # if the server sends termination message
                     os._exit(0)
                 if self.gui_mode:
-                    from kivy.clock import Clock
-                    Clock.schedule_once(lambda dt: self.gui_app.updateChatHistory(serverMessage))
+                 #   from kivy.clock import Clock
+                    Clock.schedule_once(lambda dt: self.gui_app.updateChatHistory(serverMessage)) # update messages when they arrive from kivy clock
                 else:
                     print(serverMessage)
             except:
@@ -48,8 +49,8 @@ class ClientBackend:
     def sendMessageLoop(self): # if GUI is not active
         while True:  #always wait for input to send message
             clientInput = input("") # input = what client types
-            clientMessage = self.name + ": " + clientInput
-            self.socket.send(clientMessage.encode())
+            clientMessage = self.name + ": " + clientInput # append client name and client's message
+            self.socket.send(clientMessage.encode()) # send this message down the socket
 
     def sendMessage(self, message): # if GUI is active
         clientMessage = self.name + ": " + message
@@ -57,4 +58,4 @@ class ClientBackend:
 
 
 if __name__ == '__main__': # to run main
-        ClientBackend(IP, 3232)  # create instance of client - now in clientGUI
+        ClientBackend(IP, 3232)  # create instance of client
